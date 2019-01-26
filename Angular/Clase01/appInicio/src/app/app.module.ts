98623094853639fee168bbb2c21c68a4c01e6374
alumno <alumno@admins-MacBook-Pro.local>
2019-01-26 15:29:51 -0500

    Inicio

--- Angular/Clase01/appInicio/src/app/app.module.ts
+++ Angular/Clase01/appInicio/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { MiPruebaComponent } from './miprueba.component';
 import { ErrorComponent } from './error.component';
 import { NotificacionComponent } from './notificacion.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
 	declarations: [
 		AppComponent, MiPruebaComponent,
 		ErrorComponent,
-		NotificacionComponent
+		NotificacionComponent,
+		UsuarioComponent,
+		PersonasComponent
 	],
 	imports: [
 		BrowserModule
