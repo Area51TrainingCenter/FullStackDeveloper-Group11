d76f195129ad10134bba8e4efc0780ebdeb5ea3a
alumno <alumno@Area51s-MacBook-Proo.local>
2019-03-02 11:57:53 -0500

    Forms Module - Parte final

--- Angular/Clase06/appForm/src/app/app.component.ts
+++ Angular/Clase06/appForm/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,9 +7,28 @@ import { NgForm } from '@angular/forms';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	@ViewChild("formulario") formulario: NgForm
 
-	registrar(formulario: NgForm) {
-		if (!formulario.valid) alert("Los campos no son válidos")
-		console.log(formulario)
+	ngOnInit() {
+		setTimeout(() => {
+			/* 			this.formulario.setValue({
+							nombreCompleto: "Juan Pérez",
+							correo: "juan.perez@comun.com",
+							contrasena: "123456789"
+						}) */
+
+			this.formulario.form.patchValue({
+				correo: "juan.perez@comun.com",
+				contrasena: "abcdefg"
+			})
+
+		}, 0)
+	}
+
+	registrar() {
+		console.log(this.formulario.value)
+		this.formulario.reset()
+		/* 		if (!this.formulario.valid) alert("Los campos no son válidos")
+				console.log(this.formulario) */
 	}
 }
