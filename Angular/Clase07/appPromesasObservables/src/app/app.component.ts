6d91bb7fce7f4497e7a7cbf0124d6f6d2623b3f3
Leonelson Palima <leonelsonpalima@gmail.com>
2019-03-09 13:12:24 -0500

    Observable

--- Angular/Clase07/appPromesasObservables/src/app/app.component.ts
+++ Angular/Clase07/appPromesasObservables/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,58 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  usuarios: [] = []
-  fotos: [] = []
-
-  ajax(url, resolve, reject) {
-    const xhr = new XMLHttpRequest()
-
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        resolve(JSON.parse(xhr.responseText))
-      } else if (this.readyState == 4) {
-        reject({ status: this.status, message: this.statusText })
-      }
-    }
-
-    xhr.open("get", url, true)
-    xhr.send()
-  }
+  suscripcion: Subscription
 
   ngOnInit() {
-    const promesa = new Promise((resolve, reject) => {
-      this.ajax("http://jsonplaceholder.typicode.com/posts", resolve, reject)
-    })
+    const observable: Observable<string> = Observable.create(
+      (observador: Observer<string>) => {
+        setTimeout(() => {
+          observador.next("A los 3s: Vino el del periódico")
+        }, 3000)
 
-    const promesaFotos = new Promise((resolve, reject) => {
-      this.ajax("http://jsonplaceholder.typicode.com/photos", resolve, reject)
-    })
+        setTimeout(() => {
+          observador.next("A los 5s: Alguien tocó la puerta")
+        }, 5000)
 
-    /*     Promise.all([promesa, promesaFotos])
-          .then(
-            (respuesta: any[]) => {
-              this.usuarios = respuesta[0]
-              this.fotos = respuesta[1]
-            }
-          )
-          .catch(
-            (error: any) => console.log(error)
-          ) */
+        setTimeout(() => {
+          observador.next("A los 10s: Se fue la luz")
+        }, 10000)
+      }
+    )
 
+    this.suscripcion = observable.subscribe(
+      mensaje => console.log(mensaje)
+    )
 
-    Promise.race([promesa, promesaFotos])
-      .then(
-        respuesta => console.log(respuesta)
+    setTimeout(() => {
+      observable.subscribe(
+        mensaje => console.log(mensaje)
       )
-      .catch(
-        error => console.log(error)
-      )
+    }, 6000)
 
 
 
+  }
 
-
+  ngOnDestroy() {
+    this.suscripcion.unsubscribe()
   }
 
 
