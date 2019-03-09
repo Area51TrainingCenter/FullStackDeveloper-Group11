e6b58b5cde1ebac80ebee0be80ddb1d6752ea928
Leonelson Palima <leonelsonpalima@gmail.com>
2019-03-09 11:30:44 -0500

    Promesas

--- Angular/Clase07/appPromesasObservables/src/app/app.component.ts
+++ Angular/Clase07/appPromesasObservables/src/app/app.component.ts
@@ -10,44 +10,27 @@ export class AppComponent {
   usuarios: Promise<any>
   fotos: [] = []
 
-  ngOnInit() {
-    /*     const promesa = new Promise((resolve, reject) => {
-          setTimeout(() => {
-            //resolve({ nombre: "ABC", cargo: "DEF" })
-            reject({ status: 403, message: "User not authorized" })
-          }, 1500)
-        })
+  ajax(url, resolve, reject) {
+    const xhr = new XMLHttpRequest()
 
-        promesa.then(
-          data => console.log(data),
-          error => console.log(error)
-        ) */
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        resolve(JSON.parse(xhr.responseText))
+      } else if (this.readyState == 4) {
+        reject({ status: this.status, message: this.statusText })
+      }
+    }
+
+    xhr.open("get", url, true)
+    xhr.send()
+  }
+
+  ngOnInit() {
+
 
     const promesa = new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest()
-
-      xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          resolve(JSON.parse(xhr.responseText))
-          //console.log(xhr.responseText)
-        } else if (this.readyState == 4) {
-          reject({ status: this.status, message: this.statusText })
-        }
-        //console.log("Estado", this.readyState)
-        //console.log("Estado de la respuesta", this.status)
-      }
-
-      xhr.open("get", "http://jsonplaceholder.typicode.com/posts", true)
-      xhr.send()
-
+      this.ajax("http://jsonplaceholder.typicode.com/posts", resolve, reject)
     })
-
-    /*     promesa.then(
-          (data: string) => {
-            this.usuarios = JSON.parse(data)
-          },
-          error => console.log("PROMESA RECHAZADA", error)
-        ) */
 
     this.usuarios = promesa.then()
 
@@ -56,22 +39,11 @@ export class AppComponent {
         error => console.log("PROMESA RECHAZADA", error)
       )
 
-
     const promesaFotos = new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest()
-      xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          resolve(xhr.responseText)
-        } else if (this.readyState == 4) {
-          reject({ status: this.status, message: this.statusText })
-        }
-      }
-      xhr.open("get", "http://jsonplaceholder.typicode.com/photos", true)
-      xhr.send()
+      this.ajax("http://jsonplaceholder.typicode.com/photos", resolve, reject)
     })
-
     promesaFotos.then(
-      (data: string) => this.fotos = JSON.parse(data),
+      (data: any) => this.fotos = data,
       error => console.log(error)
     )
 
