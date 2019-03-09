d46d7c99b410d7e86ee5bc189b0776c8d598d619
Leonelson Palima <leonelsonpalima@gmail.com>
2019-03-09 10:51:37 -0500

    Promesas

---
+++ Angular/Clase07/appPromesasObservables/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  usuarios: [] = []
+
+  ngOnInit() {
+    /*     const promesa = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            //resolve({ nombre: "ABC", cargo: "DEF" })
+            reject({ status: 403, message: "User not authorized" })
+          }, 1500)
+        })
+
+        promesa.then(
+          data => console.log(data),
+          error => console.log(error)
+        ) */
+
+    const promesa = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest()
+
+      xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          resolve(xhr.responseText)
+          //console.log(xhr.responseText)
+        } else if (this.readyState == 4) {
+          reject({ status: this.status, message: this.statusText })
+        }
+        //console.log("Estado", this.readyState)
+        //console.log("Estado de la respuesta", this.status)
+      }
+
+      xhr.open("get", "http://jsonplaceholder.typicode.com/posts", true)
+      xhr.send()
+
+    })
+
+    promesa.then(
+      (data: string) => {
+        this.usuarios = JSON.parse(data)
+      },
+      error => console.log("PROMESA RECHAZADA", error)
+    )
+
+
+
+
+  }
+
+
+}
