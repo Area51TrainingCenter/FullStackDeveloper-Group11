1d7578fe939e9a61c38f1f833094e6c9c1c6387f
Sergio <sergiohidalgocaceres@gmail.com>
2019-04-13 11:31:50 -0500

    Esqueleto

--- Node/Clase11/server.ts
+++ Node/Clase11/server.ts
@@ -1,5 +1,24 @@
 import express = require("express")
+import fs = require("fs")
+import path = require("path")
+import { Request, Response } from "express"
 
 const app = express()
 
-app.listen(3000, () => console.log("Servidor en el puerto 3000"))
+app.use("/assets", (req: Request, res: Response) => {
+	const ruta = path.join(__dirname, "/public/assets", req.url)
+
+	fs.exists(ruta, existe => {
+		if (existe) {
+			res.sendFile(ruta)
+		}
+	})
+
+	//console.log(req.url)
+})
+
+app.get("/", (req: Request, res: Response) => {
+	res.sendFile(__dirname + "/public/index.html")
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"))
