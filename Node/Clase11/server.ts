9e1beb7a9761f8de5ab437354abad6e1e980bfe8
Sergio <sergiohidalgocaceres@gmail.com>
2019-04-13 13:12:57 -0500

    Esqueleto

--- Node/Clase11/server.ts
+++ Node/Clase11/server.ts
@@ -3,10 +3,16 @@ import fs = require("fs")
 import path = require("path")
 import { Request, Response, NextFunction } from "express"
 
+interface IError extends Error {
+	status?: number
+}
+
 const app = express()
 
-app.use("/assets", (req: Request, res: Response, next: NextFunction) => {
-	const ruta = path.join(__dirname, "/public/assets", req.url)
+app.use(express.static("./public"))
+
+/* app.use((req: Request, res: Response, next: NextFunction) => {
+	const ruta = path.join(__dirname, "/public", req.url)
 
 	fs.exists(ruta, existe => {
 		if (existe) {
@@ -15,12 +21,56 @@ app.use("/assets", (req: Request, res: Response, next: NextFunction) => {
 			next()
 		}
 	})
-
-	//console.log(req.url)
-})
+}) */
 
 app.get("/", (req: Request, res: Response) => {
 	res.sendFile(__dirname + "/public/index.html")
+})
+
+/* app.use("/usuarios/listar", (req: Request, res: Response) => {
+	res
+		.status(401)
+		.send("Usuario no logueado")
+}) */
+
+app.get("/usuarios/listar",
+
+	(req: Request, res: Response, next: NextFunction) => {
+		/* res
+			.status(401)
+			.send("Usuario no logueado") */
+		next()
+	},
+
+	(req: Request, res: Response) => {
+		res
+			.status(200)
+			.json([
+				{ user: "user01" },
+				{ user: "user02" }
+			])
+	}
+
+)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	const err: IError = new Error("Path not found")
+	err.status = 404
+
+	next(err)
+
+	//res.status(404).send("Not Found")
+})
+
+app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
+	res
+		.status(err.status)
+		.json({
+			name: err.name,
+			status: err.status,
+			message: err.message,
+			stack: err.stack
+		})
 })
 
 app.listen(3000, () => console.log("Server running on port 3000"))
